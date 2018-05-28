46641fc964ef4a2e7e881546080bd848d1f85c50
bdimon <regsdimon-reg@yahoo.com>
2018-05-28 17:40:33 +0300

    showMore-right Way

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -21,67 +21,96 @@ export class DetailPage {
   public commentsCount: number;
   private isLoading: boolean=false;
   public comments: any=[];
-  public page:number = 1;
+  public page:number=1;
   private sort:string='1';
-  public showMore: boolean = false;
+  public showMore: boolean = true;
+  public pages: number=1;
+  public count:number=0;
   
   constructor(public navCtrl: NavController, public navParams: NavParams, public api: ApiProvider) {
     this.post = navParams.get('post');
-    this.getStatus();
+    this.getComments();
     }
     
   ionViewWillEnter() {
     
-    this.getHeaders();
+    // this.getStatus();
+    // this.getHeaders();
     }
 
-   changeSort() {
-    console.log(this.sort);
-      this.comments=[];
-      this.page=1;
-      this.getHeaders();
-    }
+  //  changeSort() {
+  //   console.log(this.sort);
+  //     this.comments=[];
+  //     this.page=1;
+  //     this.getHeaders();
+  //   }
 
-    getStatus() {
-      let url:string='comments?_envelope&post=' + this.post.id;
-      this.api.get(url).subscribe((resp:any) => {
-      if (resp.status==200) {
-        // return commentsCount = resp.headers['X-WP-Total'];
-        console.log(resp.headers['X-WP-Total']);
-        console.log(resp.headers['X-WP-TotalPages']);
-      }
-    });
-  }
+  //   getStatus() {
+  //     let url:string='comments?_envelope&post=' + this.post.id+'&page='+this.page;
+  //     this.api.get(url).subscribe((resp:any) => {
+  //     if (resp.status==200) {
+  //       // return commentsCount = resp.headers['X-WP-Total'];
+  //       console.log(resp.headers['X-WP-Total']);
+  //       console.log(resp.headers['X-WP-TotalPages']);
+  //       this.pages=(resp.headers['X-WP-TotalPages']);
+  //     }
+  //   });
+  // }
 
-   getHeaders(infiniteScroll=null) {
-    this.showMore = true;
-    if(!this.isLoading){
-      this.isLoading = true;
-      let url:string='comments?_embed&page='+this.page + '&post=' + this.post.id;
-    url += this.sort=='1'? '&order=asc#': '#';
-    console.log(url);
-    this.api.get(url).
-    subscribe((resp:any) => {
-      this.isLoading = false;
-      this.comments = this.comments.concat(resp);
-      this.page++;
-      if (this.comments.length == this.commentsCount){
-            this.showMore = false;
+  //  getHeaders(infiniteScroll=null) {
+  //   this.showMore = true;
+  //   if(!this.isLoading){
+  //     this.isLoading = true;
+  //     this.page=this.pages ;
+  //     console.log(this.page);
+  //     let url:string='comments?_embed&page='+this.page + '&post=' + this.post.id;
+  //   url += this.sort=='1'? '&order=asc#': '';
+  //   console.log(url);
+  //   this.api.get(url).
+  //   subscribe((resp:any) => {
+  //     this.isLoading = false;
+  //     this.comments = this.comments.concat(resp);
+  //     this.page--;
+  //     // if (this.comments.length == this.commentsCount){
+  //     //       this.showMore = false;
             
-            return ;            
-            }
-            else {
-            this.showMore = true;
-          }
-          if (infiniteScroll!=null){
-            infiniteScroll.complete();
-          }
+  //     //       return ;            
+  //     //       }
+  //     //       else {
+  //           this.showMore = true;
+  //           if (infiniteScroll!=null){
+  //           infiniteScroll.complete();
+  //         }
+          
+  //         }
       
-    }, (error) => {
-      this.isLoading = false;
-      this.showMore = false;
-      console.log('error');
-    });
+  //   , (error) => {
+  //     this.isLoading = false;
+  //     this.showMore = false;
+  //     console.log('error');
+  //   });
+  // }
+  // }
+
+  getComments() {
+    let url:string = 'comments?_envelope&post='+this.post.id+'&page='+this.page+'&order=asc';
+    console.log(url);
+      this.api.get(url)
+    .subscribe((resp:any) => {
+      this.comments = this.comments.concat(resp.body);
+      
+      console.log(resp.headers['X-WP-TotalPages']);
+      this.count = resp.headers['X-WP-Total'];
+      console.log(this.count);
+      this.pages = resp.headers['X-WP-TotalPages'];
+      this.page++;
+      if(resp.headers['X-WP-TotalPages']==this.page-1) {
+        this.showMore = false;
+      }
+    }
+  );
+  
+    } 
   }
-  }
-}
+  
+
